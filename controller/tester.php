3c246fe8d26cc0f80078d3955ba6643788f42f50
AbrahamGarcia <zildjianremo@gmail.com>
2018-04-05 16:52:41 +0200

    alert show info from db

--- controller/tester.php
+++ controller/tester.php
@@ -12,12 +12,19 @@
     //check if I was able to do the consult
     if($response=mysqli_query($conection,$sql))
 				{
-					echo 1;
+					//echo 1;
 				}
 				else{
 					 echo "Error: " . $sql . "<br>" . mysqli_error($conection);
 	}
-
+    //$numrows=mysqli_num_rows($response);
     
+     while($column=mysqli_fetch_array($response)){
+        $id=$column['id'];
+        $firstname=$column['firstname'];
+		$lastname=$column['lastname'];
+		$regdate=$column['reg_date'];
+    }
+    echo $id." ".$firstname." ".$lastname." ".$regdate;
 			
 ?>
