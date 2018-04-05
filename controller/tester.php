9d01716105e7362465a60f99eaf5d3cb608da3a3
AbrahamGarcia <zildjianremo@gmail.com>
2018-04-05 16:36:51 +0200

    Creation of tester.php and select clause

---
+++ controller/tester.php
@@ -0,0 +1,21 @@
+<?php
+    
+    require_once("../model/conect.php");
+    
+	$pass = md5($_POST['password']);
+    $email= $_POST['email'];
+    
+    
+
+    $sql="select * from users where password=".$pass."'and email='".$email."';";
+    echo $sql;
+    //$result=mysqli_query($conection,$sql);
+    
+    /*if(!$result){
+        echo "Error: " . $sql . "<br>" . mysqli_error($conection);
+    }
+    else{
+        echo 1;
+    }*/
+    
+?>
