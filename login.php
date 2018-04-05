bdacfb647c7538f909375cfdab1993ec476fd303
AbrahamGarcia <zildjianremo@gmail.com>
2018-04-05 16:33:33 +0200

    Front end of login finished

--- login.php
+++ login.php
@@ -29,7 +29,7 @@
 
 
             $("#btn-send").click(function(e) {
-               // alert($("#example").serialize());
+                // alert($("#example").serialize());
 
                 $.ajax({
                     type: "post",
@@ -41,7 +41,7 @@
                         if (resp == 1) {
                             $('#modal1').modal('open');
                             //alert("good");
-                            
+
                         } else {
                             //alert(resp)
                             $('#modal2').modal('open');
@@ -71,39 +71,15 @@
         </nav>
 
         <div class="parallax-container">
-            <div class="parallax"><img src="images/scott-umstattd-93198.jpg"></div>
+            <div class="parallax"><img src="images/oskar-krawczyk-172847-unsplash.jpg"></div>
         </div>
 
 
         <div class="section white">
             <div class="row container">
-                <h2 class="header">Example form with AJAX and jQuery :)</h2>
-                <p class="grey-text text-darken-3 lighten-3">Here's a little example of a form with AJAX and jQuery</p>
-
+                <h2 class="header center">Log in</h2>
                 <div class="row">
                     <form class="col s12" id="example" name="example">
-                        <div class="row">
-                            <div class="input-field col s6">
-                                <input placeholder="Placeholder" id="first_name" name="first_name" type="text" class="validate">
-                                <label for="first_name">First Name</label>
-                            </div>
-                            <div class="input-field col s6">
-                                <input id="last_name" name="last_name" type="text" class="validate">
-                                <label for="last_name">Last Name</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-                                <label for="disabled">Disabled</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input id="password" type="password" name="password" class="validate">
-                                <label for="password">Password</label>
-                            </div>
-                        </div>
                         <div class="row">
                             <div class="input-field col s12">
                                 <input id="email" type="email" class="validate" name="email">
@@ -112,8 +88,15 @@
                         </div>
 
                         <div class="row">
+                            <div class="input-field col s12">
+                                <input id="password" type="password" name="password" class="validate">
+                                <label for="password">Password</label>
+                            </div>
+                        </div>
+
+                        <div class="row">
                             <div class="col offset-s5"></div>
-                            <a class="waves-effect waves-light btn" id="btn-send"><i class="material-icons left">send</i>Send!</a>
+                            <a class="waves-effect waves-light btn" id="btn-send">Log in</a>
 
                         </div>
                     </form>
@@ -123,7 +106,7 @@
         </div>
 
         <div class="parallax-container">
-            <div class="parallax"><img src="images/photo-1495800474903-6980ebfe465a.jpg"></div>
+            <div class="parallax"><img src="images/drew-graham-547573-unsplash.jpg"></div>
         </div>
 
 
@@ -131,19 +114,10 @@
 
 
     <!--modals-->
-    <div id="modal1" class="modal">
-        <div class="modal-content">
-            <h4>Registered!</h4>
-            <p>Great!</p>
-        </div>
-        <div class="modal-footer">
-            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
-        </div>
-    </div>
 
     <div id="modal2" class="modal">
         <div class="modal-content">
-            <h4>FATAL ERROR!</h4>
+            <h4>Bad Credetials</h4>
             <p>Try again</p>
         </div>
         <div class="modal-footer">
